aabaccc1633617e6be249662a766e6d24aa61f37
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-07-29 13:00:24 +0000

    stubs for info and result

--- mockraceapi/Program.cs
+++ mockraceapi/Program.cs
@@ -1,21 +1,22 @@
 // see https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis
 
-// var builder = WebApplication.CreateBuilder(args);
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions
-{
-    Args = args,
-    // also: ASPNETCORE_APPLICATIONNAME	--applicationName
-    // ApplicationName = typeof(Program).Assembly.FullName,
-    // also: ASPNETCORE_ENVIRONMENT	--environment
-    // EnvironmentName = Environments.Staging,
-    // and then Console.WriteLine($"Environment Name: {builder.Environment.EnvironmentName}");
-    WebRootPath = "middleware"
-    // config: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/minimal-apis?view=aspnetcore-8.0#read-configuration
-});
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureHttpJsonOptions(options => {
     options.SerializerOptions.WriteIndented = true;
     options.SerializerOptions.IncludeFields = true;
+});
+builder.Services.AddCors(
+    options => {
+        options.AddDefaultPolicy(
+            policy => {
+                policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            });
+});
+builder.Logging.AddSimpleConsole(options => {
+    options.IncludeScopes = true;
+    options.SingleLine = true;
+    options.TimestampFormat = "HH:mm:ss "; // "yyyy-MM-dd HH:mm:ss zzz"
 });
 
 var app = builder.Build();
@@ -32,6 +33,20 @@ mware.MapGet("/", () =>
         ))
         .ToArray();
     return ans;
+});
+
+// /info/json?setting=courses
+// /info/json?setting=splits&course=101
+// /info/json?setting=categories&course=101
+mware.MapGet("/info/json", (string setting, int? course) => {
+    app.Logger.LogInformation("info");
+    return Results.Ok($"here are setting {setting} for course {course ?? 0}");
+});
+
+// /result/json?course=102&detail=start,gender,status&splitnr=101,109,119,199
+mware.MapGet("/result/json", (int course, string detail, string splitnr) => {
+    app.Logger.LogInformation("results");
+    return Results.Ok($"here are results for {course}");
 });
 
 app.Run();
